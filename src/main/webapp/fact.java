0ec036dd5b3523d406734c9bbb4c69b9e1e5ab6c
pnlaxmi <varadapujitha8@gmail.com>
2022-06-28 15:27:35 +0530

    first commit

---
+++ src/main/webapp/fact.java
@@ -0,0 +1,14 @@
+public class fact {
+
+    public static void main(String[] args) {
+
+        int num = 16;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
